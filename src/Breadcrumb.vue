191af565d79f7431f73cc68eee5fe1af004ac9b7
Fangdun Cai (Fundon) cfddream@gmail.com <cfddream@gmail.com>
2016-07-15 17:46:01 +0800

    Breadcrumb

---
+++ src/Breadcrumb.vue
@@ -0,0 +1,43 @@
+<template>
+  <ol class="breadcrumb">
+    <li v-for="item in list"><span class="active" v-if="isLast($index)">{{item.label}}</span>
+      <a v-link="{ name: item.link }" v-else>{{item.label}}</a>
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+
+  props: {
+    list: {
+      type: Array,
+      required: true,
+      default: () => []
+    }
+  },
+
+  methods: {
+    isLast (index) {
+      return index === this.list.length - 1
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.breadcrumb {
+  --separator: '/\00a0';
+
+  list-style: none;
+  align-items: center;
+  display: flex;
+  justify-content: flex-end;
+
+  & > li + li:before {
+    padding: 0 5px;
+    color: #ccc;
+    content: var(--separator);
+  }
+}
+</style>
